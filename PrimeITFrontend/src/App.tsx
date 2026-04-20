6272c2521e98f2de5d175dd31aade709e68873ba
Samet YILMAZ <sametyilmaz4444@gmail.com>
2026-04-20 04:26:57 +0300

    secret key gizlilikleri

--- PrimeITFrontend/src/App.tsx
+++ PrimeITFrontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 import type { RootState } from './store';
